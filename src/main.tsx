698311b0974b3340c7b5eb591f5c2044e11f1334
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-19 23:31:33 +0900

    Make main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,33 @@
-export const Main = () => {
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  title: string;
+};
+
+export const Main: React.FC = () => {
+  const { register, handleSubmit, errors, reset } = useForm<FormData>();
+  const handleOnSubmit = (data: FormData) => {
+    console.log(data.title);
+    reset();
+  };
+
   return (
-    <>
+    <div>
+      <form onSubmit={handleSubmit(handleOnSubmit)}>
+        <input
+          type="text"
+          className={errors.title && "error"}
+          name="title"
+          ref={register({ required: "タイトルは必ず入力してください。" })}
+        />
+        {errors.title && (
+          <span className="error-message">{errors.title.message}</span>
+        )}
+        <button className="btn is-primary">追加</button>
+      </form>
       <div></div>
       <div></div>
-    </>
+    </div>
   );
 };
