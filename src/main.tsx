6e0ccdeff58daefffcb5313bacc81f545c5f6292
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-20 22:53:09 +0900

    form

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ type FormData = {
 
 export const Main: React.FC = () => {
   const { register, handleSubmit, errors, reset } = useForm<FormData>();
+  //フォームが送信された時実行される関数。リセット関数でインプットに入力した値をクリアすることができる。
   const handleOnSubmit = (data: FormData) => {
     console.log(data.title);
     reset();
@@ -14,13 +15,17 @@ export const Main: React.FC = () => {
 
   return (
     <div>
+      {/* handleSubmitを設定 */}
       <form onSubmit={handleSubmit(handleOnSubmit)}>
         <input
           type="text"
+          // エラー時のinputにスタイルを設定
           className={errors.title && "error"}
           name="title"
+          //バリデーションはrelに設定。入力必須の場合設定
           ref={register({ required: "タイトルは必ず入力してください。" })}
         />
+        {/* エラーがあった時に表示するメッセージ */}
         {errors.title && (
           <span className="error-message">{errors.title.message}</span>
         )}
